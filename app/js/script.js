cddea26cfaae085117c1717e8a734425bb9ab140
Jamie Lobo <jamie.lobo@webklipper.com>
2022-11-04 17:19:17 +0530

    other artist section

--- app/js/script.js
+++ app/js/script.js
@@ -15,11 +15,43 @@ function initHeroAnimation () {
 $(document).ready(function () {
     new WOW().init();
 
-    initHeroAnimation();
+    // initHeroAnimation();
 
     $(".other-brand-carousel").slick({
         infinite: true,
         slidesToShow: 4,
+        slidesToScroll: 1,
+        autoscroll: true,
+        nextArrow: `<img class='next-arrow' src="img/carousel/right.png">`,
+        prevArrow: `<img class='prev-arrow' src="img/carousel/left.png">`
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //         slidesToShow: 3,
+        //         infinite: true
+        //         }
+
+        //     }, {
+
+        //         breakpoint: 600,
+        //         settings: {
+        //         slidesToShow: 2,
+        //         dots: true
+        //         }
+
+        //     }, {
+
+        //         breakpoint: 300,
+        //         settings: "unslick" // destroys slick
+
+        //     }
+        // ]
+    });
+
+    $(".diff-artist-carousel").slick({
+        infinite: true,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoscroll: true,
         nextArrow: `<img class='next-arrow' src="img/carousel/right.png">`,
