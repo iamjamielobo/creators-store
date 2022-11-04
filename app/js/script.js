60994e3382c640393f36570f35802016bd832a51
Jamie Lobo <jamie.lobo@webklipper.com>
2022-11-04 17:35:10 +0530

    other artist section images added

--- app/js/script.js
+++ app/js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoscroll: true,
+        autoplay: true,
         nextArrow: `<img class='next-arrow' src="img/carousel/right.png">`,
         prevArrow: `<img class='prev-arrow' src="img/carousel/left.png">`
         // responsive: [
@@ -53,7 +53,7 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoscroll: true,
+        autoplay: true,
         nextArrow: `<img class='next-arrow' src="img/carousel/right.png">`,
         prevArrow: `<img class='prev-arrow' src="img/carousel/left.png">`
         // responsive: [
