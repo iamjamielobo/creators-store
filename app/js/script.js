b5a751816e289d82ee730b91cfc0b6fb3c490295
Jamie Lobo <jamie.lobo@webklipper.com>
2022-11-04 14:28:39 +0530

    init

---
+++ app/js/script.js
@@ -0,0 +1,50 @@
+function initHeroAnimation () {
+    var $body = $('body');
+    var $heroVideo = $('.js-video');
+    var $animationDiv = $('.intro-animation');
+
+    setTimeout(function () {
+        $animationDiv.addClass('infinity');
+        setTimeout(function () {
+            $body.removeClass('no-scroll');
+            $heroVideo.get(0).play();
+        }, 500);
+    }, 3000);
+}
+
+$(document).ready(function () {
+    new WOW().init();
+
+    initHeroAnimation();
+
+    $(".other-brand-carousel").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: `<img class='next-arrow' src="/img/carousel/right.png">`,
+        prevArrow: `<img class='prev-arrow' src="/img/carousel/left.png">`
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //         slidesToShow: 3,
+        //         infinite: true
+        //         }
+
+        //     }, {
+
+        //         breakpoint: 600,
+        //         settings: {
+        //         slidesToShow: 2,
+        //         dots: true
+        //         }
+
+        //     }, {
+
+        //         breakpoint: 300,
+        //         settings: "unslick" // destroys slick
+
+        //     }
+        // ]
+    });
+})
